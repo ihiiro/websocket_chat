981cf13d166a7b645ab5e41599edde1accb13500
Hiro <yassirelyakine@gmail.com>
2023-01-23 19:42:02 +0100

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,15 +1,15 @@
-const ws = new WebSocket('ws://0.0.0.0:8080');
+const wss = new WebSocket(`wss://${processs.env.HOST}:${process.env.PORT}`);
 
-ws.addEventListener('open', () => {
+wss.addEventListener('open', () => {
   document.getElementById('send').addEventListener('click', () => {
     // convert object into json string so the data makes it in the correct format
-    ws.send(JSON.stringify({
+    wss.send(JSON.stringify({
       sender: document.getElementById('sender').value,
       message: document.getElementById('message').value,
     }));
   });
 
-  ws.addEventListener('message', data => {
+  wss.addEventListener('message', data => {
     const message_object = JSON.parse(data.data);
     document.getElementById('chat-history').innerHTML += `<div class="chat-message">
       <span class="sender-username">${message_object.sender}:</span>
